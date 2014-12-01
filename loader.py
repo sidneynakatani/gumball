caa86647cc9e530bdf40c47eaf9240f22d2841f2
koding user <sidney.nakatani@hotmail.com>
2014-12-01 23:21:53 +0000

    adjust

--- loader.py
+++ loader.py
@@ -1,10 +1,19 @@
 from flask import Flask, request, jsonify, redirect
+from util.balancer import Balancer
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
+    b1 = Balancer()
+    print b1.host_selector()
+
+    b1.change_host()
+
+    b2 = Balancer()
+    print b2.host_selector()
+
     return 'Hello!'
 
 
