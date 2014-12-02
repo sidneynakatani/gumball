7060262bc271a8146e6a304abcb09bec7b7d899a
koding user <sidney.nakatani@hotmail.com>
2014-12-02 18:08:24 +0000

    test

--- loader.py
+++ loader.py
@@ -1,22 +1,25 @@
 from flask import Flask, request, jsonify, redirect
 from util.balancer import Balancer
 from flask.ext.noextref import NoExtRef
+#from flaskext.noextref import NoExtRef
 
 app = Flask(__name__)
 b1 = Balancer()
-noext = NoExtRef(app)
+app.test_request_context().push()
 
 @app.route('/')
 def home():
     
     host =  b1.host_selector()
     b1.change_host()
-    return redirect(host, 301) 
+    return redirect(host, 302) 
 
 
-@app.route('/teste')
 def teste():
-    noext.hide_url('http://google.com')
+#     return noext.hide_url('http://uol.com.br')
+    return redirect('http://uol.com.br')
+
+noext = NoExtRef(app, rule='/go/', view_func=teste)
 
 if __name__ == '__main__':
     app.run(debug=True)
